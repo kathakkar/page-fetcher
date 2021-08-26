4574ecb5d5fa830a055b114cbeb2c7acc62c34b0
Khushbu <khushbu.thakkar@outlook.com>
2021-08-26 17:22:41 +0000

    saving file with changes

--- fetcher.js
+++ fetcher.js
@@ -1,38 +1,42 @@
 arr = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs')
-//downloading file
-request(arr[0], (error, response, body) => { 
-  
+
+const writeToFile = function (filename,body,error){
+
   if(!error){
-    //writing to fle
-    fs.writeFile(arr[1], body, err => {
-      if (err) {
-        console.log(err)
-        return
-      }
-      else{
-        //checking file size
-        fs.stat(arr[1], (err, fileStats) => {
+        //writing to fle
+        fs.writeFile(filename, body, err => {
           if (err) {
             console.log(err)
-          } else {
-            //file written successfully
-             console.log(`Downloaded and saved ${fileStats.size} bytes to ${arr[1]}`);
+            return
           }
-        });  
-      }
+          else{
+            //checking file size
+            fs.stat(filename, (err, fileStats) => {
+              if (err) {
+                console.log(err)
+              } else {
+                //file written successfully
+                 console.log(`Downloaded and saved ${fileStats.size} bytes to ${filename}`);
+              }
+            });  
+          }
+        
+        });
     
-    });
+      } else {
+        console.log('error:', error); // Print the error if one occurred
+      }
 
-  } else {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  }
-  
-});
+}
 
 
+const newFun = function (url,filename,callback){
+  //downloading file
+  request(url, (error, response, body) => { 
+    callback(filename,body,error);
+  });
+}
 
-const content = 'Some content!'
-
+newFun(arr[0],arr[1],writeToFile);
